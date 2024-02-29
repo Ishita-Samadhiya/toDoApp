fb16504405c8a9265ad10bc15d4b9bbe9d246d95
Ishita-Samadhiya <i.samadhiya@my.metroed.net>
2024-02-29 08:12:27 -0800

    SLay

---
+++ toDo/toDo/toDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoApp.swift
+//  toDo
+//
+//  Created by StudentAM on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct toDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
